4b20e647071a4c27132519a0cbf21476462a71e2
quynhthu <146862065+quynhthunh@users.noreply.github.com>
2023-11-10 23:08:02 +0700

    44.py

--- 44.py
+++ 44.py
@@ -1,10 +1,10 @@
 ngay=int(input("Nhap ngay: "))
 thang=input("Nhap thang: ")
-if ngay==1 and thang=="Thang Mot":
+if ngay==1 and thang==1:
     print("Day la ngay dau nam moi.")
-elif ngay==1 and thang=="Thang Bay":
+elif ngay==1 and thang==7:
     print("Day la ngay Quoc khanh nuoc Canada.")
-elif ngay==25 and thang=="Thang Muoi Hai":
+elif ngay==25 and thang==12:
     print("Day la ngay le giang sinh.")
 else:
-    print("Ngay va thang khong hop le. Vui long nhap lai.")
+    print("Ngay va thang khong hop le.")
